5a6062ac6c88cce0a641323fda2a03bb8a19573f
Norgeth <111367440+Norgeth@users.noreply.github.com>
2023-06-03 22:43:20 +0200

    added border option and text

--- 3/code.js
+++ 3/code.js
@@ -1,65 +1,82 @@
-console.log("hello");
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
-
 const balls = [];
-let count, x, y, radius, color, speedX, speedY;
+let isStrokeActive = false;
 
-//chatgpt
-const getRandomInt = (min,max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const getRandomColor = () => {
-//CHAT GPT
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+const getRandomInt = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-  do {
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-  } while (color === '#FFFFFF'); // Exclude white color
-  return color
+  const getRandomColor = () => {
+    const hue = getRandomInt(0,360);
+    const saturation = 100;
+    const lightness = Math.floor(Math.random() * 30) + 50; // random lightness (50-80)
+    const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    return color;
+  };
+
+const addStroke = (e) => {
+  if(e.code == "Space"){
+    isStrokeActive = !isStrokeActive;
+  }
 }
-//chatgpt
 
 const drawBall = (x,y,radius,color) => {
     context.beginPath();
+    context.lineWidth = 3;
     context.arc(x,y,radius,0,2*Math.PI);
     context.fillStyle = color;
     context.fill();
     context.closePath();
+    if(isStrokeActive){
+      context.stroke();
+    }
+}
+
+const drawText = () => {
+    context.beginPath();
+    context.fillStyle = "lightgrey"
+    context.strokeStyle = "black";
+    context.font = "100px arial";
+    context.textAlign = "center";
+    context.lineWidth = 4;
+    const text = "Use spacebar to add border";
+    const x = window.innerWidth/2;
+    const y = window.innerHeight-80;
+    context.fillText(text,x,y);
+    context.strokeText(text,x,y);
+    context.closePath();
 }
 
 const createBalls = () => {
-    count = getRandomInt(10,100);
-    for(let i = 0; i<count; i++){
-        radius = getRandomInt(10,50); 
-        speedX = (getRandomInt(-25,25)/(radius/5));
-        speedY = (getRandomInt(-25,25)/(radius/5));
-        x = getRandomInt(radius,window.innerWidth-radius);
-        y = getRandomInt(radius,window.innerHeight-radius);
-        color = getRandomColor();
-        drawBall(x,y,radius,color);
-        balls.push({x,y,radius,color,speedX,speedY});  
-    }  
+    count = getRandomInt(20,100);
+      for(let i = 0; i<count; i++){
+          const radius = getRandomInt(20,100);
+          const mass = 100/radius;
+          const speedX = (getRandomInt(-50,50)/radius)*mass;
+          const speedY = (getRandomInt(-50,50)/radius)*mass;
+          const x = getRandomInt(radius,window.innerWidth-radius);
+          const y = getRandomInt(radius,window.innerHeight-radius);
+          const color = getRandomColor();
+          balls.push({x,y,radius,color,speedX,speedY});
+      }  
 }
+
 createBalls();
 
 const update = () => {
-     for(let i = 0; i<count; i++){
-        drawBall(balls[i].x,balls[i].y,balls[i].radius,balls[i].color);
-        balls[i].x += balls[i].speedX
-        balls[i].y += balls[i].speedY
-        if(balls[i].x>window.innerWidth-(balls[i].radius)||balls[i].x<0+(balls[i].radius)){
-          balls[i].speedX*=-1
-        }
-        if(balls[i].y>window.innerHeight-(balls[i].radius)||balls[i].y<0+(balls[i].radius)){
-            balls[i].speedY*=-1
-         }      
+    for(let i = 0; i<count; i++){
+       drawBall(balls[i].x,balls[i].y,balls[i].radius,balls[i].color);
+       balls[i].x += balls[i].speedX;
+       balls[i].y += balls[i].speedY;
+       if(balls[i].x>window.innerWidth-(balls[i].radius)||balls[i].x<0+(balls[i].radius)){
+          balls[i].speedX*=-1;
+       }
+       if(balls[i].y>window.innerHeight-(balls[i].radius)||balls[i].y<0+(balls[i].radius)){
+         balls[i].speedY*=-1;
+       }      
     }
+    drawText();
 }
 
 const render = () => {
@@ -69,3 +86,4 @@ const render = () => {
 }
 
 requestAnimationFrame(render);
+window.addEventListener("keydown", addStroke);
