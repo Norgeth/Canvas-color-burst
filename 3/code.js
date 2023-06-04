6c37bdf9a3574ade484fdc60da8acde201f3e359
Norgeth <111367440+Norgeth@users.noreply.github.com>
2023-06-04 14:39:46 +0200

    added deltaTime

--- 3/code.js
+++ 3/code.js
@@ -4,6 +4,7 @@ context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
 const balls = [];
 let isStrokeActive = false;
+let startTime = performance.now();
 
 const getRandomInt = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -27,15 +28,16 @@ const drawBall = (x,y,radius,color) => {
     context.arc(x,y,radius,0,2*Math.PI);
     context.fillStyle = color;
     context.fill();
-    context.closePath();
     if(isStrokeActive){
-      context.stroke();
+    context.stroke();
     }
+    context.closePath();
+    
 }
 
 const drawText = () => {
     context.beginPath();
-    context.fillStyle = "lightgrey"
+    context.fillStyle = ""
     context.strokeStyle = "black";
     context.font = "100px arial";
     context.textAlign = "center";
@@ -49,12 +51,12 @@ const drawText = () => {
 }
 
 const createBalls = () => {
-    count = getRandomInt(20,100);
+    count = getRandomInt(100,100);
       for(let i = 0; i<count; i++){
           const radius = getRandomInt(20,100);
           const mass = 100/radius;
-          const speedX = (getRandomInt(-50,50)/radius)*mass;
-          const speedY = (getRandomInt(-50,50)/radius)*mass;
+          const speedX = (getRandomInt(-50,50))*mass;
+          const speedY = (getRandomInt(-50,50))*mass;
           const x = getRandomInt(radius,window.innerWidth-radius);
           const y = getRandomInt(radius,window.innerHeight-radius);
           const color = getRandomColor();
@@ -64,11 +66,16 @@ const createBalls = () => {
 
 createBalls();
 
-const update = () => {
+const draw = () => {
+  context.clearRect(0,0,window.innerWidth,window.innerHeight);
+  balls.forEach(ball => {drawBall(ball.x, ball.y, ball.radius, ball.color)})
+  drawText();
+}
+
+const update = (deltaTime) => {
     for(let i = 0; i<count; i++){
-       drawBall(balls[i].x,balls[i].y,balls[i].radius,balls[i].color);
-       balls[i].x += balls[i].speedX;
-       balls[i].y += balls[i].speedY;
+       balls[i].x += balls[i].speedX*deltaTime;
+       balls[i].y += balls[i].speedY*deltaTime;
        if(balls[i].x>window.innerWidth-(balls[i].radius)||balls[i].x<0+(balls[i].radius)){
           balls[i].speedX*=-1;
        }
@@ -76,12 +83,13 @@ const update = () => {
          balls[i].speedY*=-1;
        }      
     }
-    drawText();
 }
 
-const render = () => {
-    context.clearRect(0,0,window.innerWidth,window.innerHeight);
-    update();
+const render = (currentTime) => {
+    let deltaTime = (currentTime-startTime)/1000;
+    startTime = currentTime;
+    draw();
+    update(deltaTime);
     requestAnimationFrame(render);
 }
 
